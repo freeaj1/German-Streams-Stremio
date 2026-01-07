9d228f10b2b9b9ca508cd15acdd6d177a98b8b5b
freeaj1 <87078248+freeaj1@users.noreply.github.com>
2026-01-07 04:38:22 +0000

    Server.js

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const addonInterface = require('./addon');
 
 const PORT = process.env.PORT || 7000;
 
-// Add health check endpoint for deployment platforms
-const express = require('express');
-const app = express();
-
-// Health check endpoint
-app.get('/', (req, res) => {
-    res.send('German Streams Pro - Addon is running! Add /manifest.json to install in Stremio');
-});
-
 // Serve the addon
 serveHTTP(addonInterface, { port: PORT });
 
